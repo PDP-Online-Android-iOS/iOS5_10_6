984b4111244331a08ebf35658e90c0f6a9fad4a7
OgabekDev <ogabekdev@gmail.com>
2022-11-30 19:26:17 +0500

    All Done

--- Task 6/ContentView.swift
+++ Task 6/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email = ""
+    @State var phone = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .top, endPoint: .bottom)
+            
+            VStack {
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.white)
+                    TextField("Enter your email", text: $email)
+                }
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 1))
+                
+                HStack {
+                    Image(systemName: "lock")
+                        .foregroundColor(.white)
+                    TextField("Enter your email", text: $email)
+                }
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 1))
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .padding()
+                })
+                .frame(maxWidth: .infinity)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 1))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            
+            VStack {
+                Spacer()
+                Text("All rights reserved")
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
